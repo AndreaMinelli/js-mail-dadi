b777cf4f874ef1e95dfe44ae60e587600ad93a91
AndreaMinelli <minelliandrea94@gmail.com>
2022-12-12 15:27:58 +0100

    Creati elementi in pagina e targhettizzati

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-console.log('JS OK')
+//# ----------- PRIMO ESERCIZIO ----------
+
+/*
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi fa il punteggio più alto.
+Stampiamo in pagina i due tiri e il risultato
+*/
+
+//Targhettizzo elementi in pagina
+
+const userNumber = document.getElementById('user-number');
+const aiNumber = document.getElementById('ai-number');
+
